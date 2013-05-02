3f69c00226f6112aed09b6db2356524b06d89660
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-05-01 20:49:45 -0400

    misc

--- paper/plots/fig-horseshoe1.R
+++ paper/plots/fig-horseshoe1.R
@@ -1,15 +1,7 @@
-library(ggplot2)
-library(reshape2)
+source("conf.R")
 library(VGAM)
 
-# Approximation of horseshoe density
-# Theorem 1 of Carvallo et al.
-dhp <- function(x) {
-  K <- 1 / sqrt(2 * pi^3)
-  lb <- (K / 2) * log(1 + 4 / x^2)
-  ub <- K * log(1 + 2 / x^2)
-  (lb + ub) / 2
-}
+OUTFILE <- commandArgs(TRUE)[1]
 
 x <- seq(-3.5, 3.5, by=0.1)
 hp <- dhp(x)
@@ -24,11 +16,11 @@ dist <- melt(data.frame(x = x,
 
 gg1 <- (ggplot(dist, aes(x = x, y = value, linetype=variable))
         + geom_line()
-        + theme_gray(base_size = 10)
+        + theme_local(base_size = 10)
         + scale_x_continuous("Density")
         + scale_y_continuous("alpha")
         + scale_linetype_discrete("")
-        + theme(legend.position = "bottom")
         + guides(linetype = guide_legend(nrow=2)))
-ggsave(plot = gg1, file = "horseshoe1.pdf",
-       width=2.5, height = 2.5)
+
+ggsave(plot = gg1, file = OUTFILE, width=2.5, height = 2.5)
+       
