2959f708657da950f441c3913f7d4b46d65700dc
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-05-03 15:03:17 -0400

    update summary

--- R/summary_nile.R
+++ R/summary_nile.R
@@ -16,56 +16,75 @@ disc_chisq <- function(y, yrep) {
   sum((y - ey)^2 / vy)
 }
 
+pr_positive <- function(x) sum(x > 0) / length(x)
+
+pr_negative <- function(x) sum(x < 0) / length(x)
+
+## pr_nonzero <- function(x) {
+##   2 * (max(pr_positive(x), pr_negative(x)) - 0.5)
+## }
+
 summary_mcmcdb_nile_normal <- function(object, y) {
+  ret <- list()
   f <- function(x) rnorm(length(x$theta), x$theta, x$sigma)
   yrep <- simplify2array(mcmcdb_samples_iter(object, FUN = f))
-  yhat <- apply(yrep, 1, mean)
-  llik <- object[["llik"]]
-  lppd <- log(apply(exp(object[["llik"]]), 1, mean))
-  waic <- waic(llik)
-  mse <- disc_mse(y, yrep)
-  chisq <- disc_chisq(y, yrep)
-  w <- 1 - mean(1 / (1 + object[["tau"]]))
-  errors <- apply(object[["theta"]], 2, `-`, y = y)
+  ret[["yrep"]] <- yrep
+  ret[["yhat"]] <- apply(yrep, 1, mean)
+  ret[["lppd"]] <- log(apply(exp(object[["llik"]]), 1, mean))
+  ret[["waic"]] <- waic(object[["llik"]])
+  ret[["mse"]] <- disc_mse(y, yrep)
+  ret[["chisq"]] <- disc_chisq(y, yrep)
+  ret[["w"]] <- 1 - mean(1 / (1 + object[["tau"]]))
+  ret[["errors"]] <- apply(object[["theta"]], 2, `-`, y = y)
   innovations <- apply(object[["theta"]], 2, diff)
-  list(yrep = yrep, yhat = yhat, lppd = lppd, waic = waic,
-       chisq = chisq, mse = mse, w = w,
-       errors = errors, innovations = innovations)
+  ret[["innovations"]] <- innovations
+  ret[["ppos"]] <- apply(innovations, 1, pr_positive)
+  ret[["pneg"]] <- apply(innovations, 1, pr_negative)
+  ret[["hpd95"]] <- apply(innovations, 1, hpd_interval, prob=0.95)
+  ret[["hpd75"]] <- apply(innovations, 1, hpd_interval, prob=0.75)
+  ret
 }
 
 summary_mcmcdb_nile_normal2 <- function(object, y) {
+  ret <- list()
   f <- function(x) rnorm(length(x$yhat), x$yhat, x$sigma)
   yrep <- simplify2array(mcmcdb_samples_iter(object, FUN = f))
-  yhat <- apply(yrep, 1, mean)
-  llik <- object[["llik"]]
-  lppd <- log(apply(exp(object[["llik"]]), 1, mean))
-  waic <- waic(llik)
-  mse <- disc_mse(y, yrep)
-  chisq <- disc_chisq(y, yrep)
-  w <- 1 - mean(1 / (1 + object[["tau"]]))
-  errors <- apply(object[["yhat"]], 2, `-`, y = y)
-  innovations <- apply(object[["theta"]], 2, diff)
-  list(yrep = yrep, yhat = yhat, lppd = lppd, waic = waic,
-       chisq = chisq, mse = mse, w = w,
-       errors = errors, innovations = innovations)
+  ret[["yrep"]] <- yrep
+  ret[["yhat"]] <- apply(yrep, 1, mean)
+  ret[["lppd"]] <- log(apply(exp(object[["llik"]]), 1, mean))
+  ret[["waic"]] <- waic(object[["llik"]])
+  ret[["mse"]] <- disc_mse(y, yrep)
+  ret[["chisq"]] <- disc_chisq(y, yrep)
+  ret[["w"]] <- 1 - mean(1 / (1 + object[["tau"]]))
+  ret[["errors"]] <- apply(object[["yhat"]], 2, `-`, y = y)
+  innovations <- apply(object[["yhat"]], 2, diff)
+  ret[["innovations"]] <- innovations
+  ret[["ppos"]] <- apply(innovations, 1, pr_positive)
+  ret[["pneg"]] <- apply(innovations, 1, pr_negative)
+  ret[["hpd95"]] <- apply(innovations, 1, hpd_interval, prob=0.95)
+  ret[["hpd75"]] <- apply(innovations, 1, hpd_interval, prob=0.75)
+  ret
 }
 
-
 summary_mcmcdb_nile_hs <- function(object, y) {
+  ret <- list()
   f <- function(x) rnorm(length(x$theta), x$theta, x$sigma)
   yrep <- simplify2array(mcmcdb_samples_iter(object, FUN = f))
-  yhat <- apply(yrep, 1, mean)
-  llik <- object[["llik"]]
-  lppd <- log(apply(exp(object[["llik"]]), 1, mean))
-  waic <- waic(llik)
-  mse <- disc_mse(y, yrep)
-  chisq <- disc_chisq(y, yrep)
-  w <- 1 - apply(object[["kappa"]], 1, mean)
-  errors <- apply(object[["theta"]], 2, `-`, y = y)
+  ret[["yrep"]] <- yrep
+  ret[["yhat"]] <- apply(yrep, 1, mean)
+  ret[["lppd"]] <- log(apply(exp(object[["llik"]]), 1, mean))
+  ret[["waic"]] <- waic(object[["llik"]])
+  ret[["mse"]] <- disc_mse(y, yrep)
+  ret[["chisq"]] <- disc_chisq(y, yrep)
+  ret[["w"]] <- 1 - apply(object[["kappa"]], 1, mean)
+  ret[["errors"]] <- apply(object[["theta"]], 2, `-`, y = y)
   innovations <- apply(object[["theta"]], 2, diff)
-  list(yrep = yrep, yhat = yhat, lppd = lppd, waic = waic,
-       chisq = chisq, mse = mse, w = w,
-       errors = errors, innovations = innovations)
+  ret[["innovations"]] <- innovations
+  ret[["ppos"]] <- apply(innovations, 1, pr_positive)
+  ret[["pneg"]] <- apply(innovations, 1, pr_negative)
+  ret[["hpd95"]] <- apply(innovations, 1, hpd_interval, prob=0.95)
+  ret[["hpd75"]] <- apply(innovations, 1, hpd_interval, prob=0.75)
+  ret
 }
 
 summaries <- 
