0b95ef5f3a6547d8b69fa68b42bdc02773a5ba22
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-06-06 12:04:08 -0400

    cleaning up implementation section

---
+++ R/sddlm/R/DlmLocalLevelNormal-class.R
@@ -0,0 +1,8 @@
+#' @include DLM-class.R
+#' @exportClass DlmLocalLevelNormal
+NULL
+
+#' @rdname DLM-class
+#' @aliases DlmLocalLevelNormal-class
+setClass("DlmLocalLevelNormal", contains = "DLM")
+
