09724954fb38559b0068ecdf5409afdc36c30536
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-08-03 23:53:16 -0400

    cleaning

---
+++ R/scripts/battles.R
@@ -0,0 +1,5 @@
+battles <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "cdb90_battles.csv"))
+
+combatants <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "cdb90_battles.csv"))
+
+durations <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "cdb90_battles.csv"))
