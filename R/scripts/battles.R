0e21f71611323caf0bdf592eacb1f13388760d79
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-08-04 03:07:03 -0400

    update

--- R/scripts/battles.R
+++ R/scripts/battles.R
@@ -1,3 +1,18 @@
 battles <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "battles.csv"))
 combatants <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "combatants.csv"))
-durations <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "active_periods.csv"))
+atp <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "active_periods.csv"))
+
+logp1 <- function(x) log(x + 1)
+
+foo <- mutate(dcast(melt(combatants, id.vars = c("isqno", "attacker"),
+                         measure.vars = c("str", "cas")),
+                    isqno ~ variable + attacker, value.var = "value"),
+              log_cas_ratio = logp1(cas_TRUE) - logp1(cas_FALSE),
+              log_str_ratio = logp1(str_TRUE) - logp1(str_FALSE))
+
+
+ddply(durations, "isqno",
+      function(x) {
+        (start_time_min + start_time_max) / 2
+      })
+      
