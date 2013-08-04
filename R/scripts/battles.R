e7afc527b3f9cff7fbe3fe4548adc9081e66031c
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-08-04 02:31:27 -0400

    update CDB13

--- R/scripts/battles.R
+++ R/scripts/battles.R
@@ -1,5 +1,3 @@
-battles <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "cdb90_battles.csv"))
-
-combatants <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "cdb90_battles.csv"))
-
-durations <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "cdb90_battles.csv"))
+battles <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "battles.csv"))
+combatants <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "combatants.csv"))
+durations <- read.csv(file.path(ROOT_DIR, "lib", "CDB13", "data", "active_periods.csv"))
